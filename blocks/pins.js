1cabd660bd7f69a65c0af6a725f1fc1401b5c6e9
sanby <sanby@matalab.com>
2018-08-14 20:07:08 +0800

    no message

---
+++ blocks/pins.js
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Visual Blocks Editor
+ *
+ * Copyright 2012 Google Inc.
+ * https://developers.google.com/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Colour blocks for Blockly.
+ *
+ * This file is scraped to extract a .json file of block definitions. The array
+ * passed to defineBlocksWithJsonArray(..) must be strict JSON: double quotes
+ * only, no outside references, no functions, no trailing commas, etc. The one
+ * exception is end-of-line comments, which the scraper will remove.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.pins');  // Deprecated
+goog.provide('Blockly.Constants.Pins');  // deprecated, 2018 April 5
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly');
+
+
+/**
+ * Unused constant for the common HSV hue for all blocks in this category.
+ * @deprecated Use Blockly.Msg['COLOUR_HUE']. (2018 April 5)
+ */
+Blockly.Constants.Pins.HUE = 66;
+
+Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
+  {
+    "type": "pins_set",
+    "message0": "setpin %1 to %2",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "MODE",
+        "options": [["GPIO0", "0"], ["GPIO1", "1"], ["GPIO2", "2"]]
+      },
+      {
+        "type": "input_value",
+        "name": "VALUE",
+        "check": "Boolean"
+      }
+    ],
+    
+    "colour": "%{BKY_PINS_HUE}",
+    "previousStatement": null,
+    "nextStatement": null,
+  }
+]);  // END JSON EXTRACT (Do not delete this comment.)
