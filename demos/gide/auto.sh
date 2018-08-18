d61b2f9e509d7e8ebcf64439c0ba2884848a5808
tongjinlv <179990830@qq.com>
2018-08-18 08:00:04 +0000

    gide

---
+++ demos/gide/auto.sh
@@ -0,0 +1,12 @@
+java -jar closure-compiler.jar --js='main.js' \
+  --js='../../blocks/**.js' \
+  --js='../../core/**.js' \
+  --js='../../generators/**.js' \
+  --js='../../msg/js/**.js' \
+  --js='../../../closure-library/closure/goog/**.js' \
+  --js='../../../closure-library/third_party/closure/goog/**.js' \
+  --generate_exports \
+  --externs ../../externs/svg-externs.js \
+  --compilation_level ADVANCED_OPTIMIZATIONS \
+  --dependency_mode=STRICT --entry_point=Main \
+  --js_output_file main_compressed.js
