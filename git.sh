456dfb1693c6eaf848285c5747d1c97fccc49fbe
tongjinlv <179990830@qq.com>
2018-08-21 02:15:50 +0000

    add

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@ git config --global credential.helper store
 git config --global user.email "179990830@qq.com"
 git config --global user.name "tongjinlv"
 git add --all
-git commit -m $0
+git commit -m $1
 git push
