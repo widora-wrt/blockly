e0682a3753547fb0f196bc9e38f3499dd1d5e8fb
tongjinlv <179990830@qq.com>
2018-12-23 10:20:16 +0800

    no message

--- makeweb.sh
+++ makeweb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+rm -rf appengine/static/demos/*
 cp -R demos/code appengine/static/demos/
+rm -rf appengine/static/demos/code/blocks
+rm -rf appengine/static/demos/code/generator
 cp -r msg/js appengine/static/msg/
 cp msg/messages.js appengine/static/msg/
 cp *_compressed.js appengine/static/
